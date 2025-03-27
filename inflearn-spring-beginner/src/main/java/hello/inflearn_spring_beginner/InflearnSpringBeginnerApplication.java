48f5944cd3b97eee85082c5ec16a4dbe6ea48770
mukkamukka <rlaehdrjs4605@gmail.com>
2025-03-27 22:11:31 +0900

    Initial commit

---
+++ inflearn-spring-beginner/src/main/java/hello/inflearn_spring_beginner/InflearnSpringBeginnerApplication.java
@@ -0,0 +1,13 @@
+package hello.inflearn_spring_beginner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InflearnSpringBeginnerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InflearnSpringBeginnerApplication.class, args);
+	}
+
+}
---
+++ inflearn-spring-beginner/src/test/java/hello/inflearn_spring_beginner/InflearnSpringBeginnerApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.inflearn_spring_beginner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InflearnSpringBeginnerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
