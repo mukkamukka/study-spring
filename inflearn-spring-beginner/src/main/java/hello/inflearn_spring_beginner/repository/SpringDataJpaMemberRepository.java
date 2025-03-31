99801b3d476732147a2027813e6a877cccd22a60
mukkamukka <rlaehdrjs4605@gmail.com>
2025-03-31 17:15:17 +0900

    섹션 7. 스프링 DB 접근 기술

---
+++ inflearn-spring-beginner/src/main/java/hello/inflearn_spring_beginner/repository/SpringDataJpaMemberRepository.java
@@ -0,0 +1,12 @@
+package hello.inflearn_spring_beginner.repository;
+
+import hello.inflearn_spring_beginner.domain.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface SpringDataJpaMemberRepository extends JpaRepository<Member, Long>, MemberRepository {
+
+    @Override
+    Optional<Member> findByName(String name);
+}
